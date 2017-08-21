dc9a23778a5111a0e3e4fc664f5bab9c77ce7d2d
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-08-20 17:30:23 -0700

    Added account.payment memo to the payment tree as well as fixed the invoiced button

---
+++ models/partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+@api.model
+def open_partner_history(self):
+    action = self.env.ref('account.action_invoice_refund_out_tree').read()[0]
+    action['domain'] = literal_eval(action['domain'])
+    action['domain'].append(('partner_id','child of',self.commercial_partner_id.id))
+    return action
