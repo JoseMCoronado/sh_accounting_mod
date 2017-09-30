eefdf1ad48572265b1bd181db550561a4fec2968
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-09-30 10:11:07 -0700

    Added indent to partner.py for the open_history method

--- models/partner.py
+++ models/partner.py
@@ -6,9 +6,9 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-@api.model
-def open_partner_history(self):
-    action = self.env.ref('account.action_invoice_refund_out_tree').read()[0]
-    action['domain'] = literal_eval(action['domain'])
-    action['domain'].append(('partner_id','child of',self.commercial_partner_id.id))
-    return action
+    @api.model
+    def open_partner_history(self):
+        action = self.env.ref('account.action_invoice_refund_out_tree').read()[0]
+        action['domain'] = literal_eval(action['domain'])
+        action['domain'].append(('partner_id','child of',self.commercial_partner_id.id))
+        return action
