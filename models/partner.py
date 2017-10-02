bee22a2ac9bf0542721af6da78946afdce4d84f3
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-10-01 21:54:56 -0700

    update3

--- models/partner.py
+++ models/partner.py
@@ -6,7 +6,7 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    @api.model
+    
     def open_partner_history(self):
         action = self.env.ref('account.action_invoice_refund_out_tree').read()[0]
         action['domain'] = literal_eval(action['domain'])
