739219b25af71343323ca89017ec85c74c9d0eee
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-10-01 21:51:42 -0700

    update2

--- models/partner.py
+++ models/partner.py
@@ -10,5 +10,5 @@ class ResPartner(models.Model):
     def open_partner_history(self):
         action = self.env.ref('account.action_invoice_refund_out_tree').read()[0]
         action['domain'] = literal_eval(action['domain'])
-        action['domain'].append(('partner_id','child of',self))
+        action['domain'].append(('partner_id','child of',self.commercial_partner_id.ids))
         return action
