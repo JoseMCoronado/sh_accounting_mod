36cfda51cbf53b9c1a6edf21e1081bdea57c6fd6
Jose M. Coronado <j.coronado.personal@gmail.com>
2018-07-22 18:48:40 -0700

    [IMP] Added send by mail button to vendor bill

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -12,6 +12,36 @@ class AccountInvoice(models.Model):
     compute='_get_purchase_order', readonly=True,
     store=True)
 
+    @api.multi
+    def action_send_bill(self):
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = dict()
+        ctx.update({
+            'default_model': 'account.invoice',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'default_partner_ids': [(6,0,[self.partner_id.id])]
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
+
+
     @api.one
     @api.depends('origin')
     def _get_purchase_order(self):
